a12d1c07ad37f32d6a23a07ebb3ba79272e9e047
Andrés Bitto <anbitto@gmail.com>
2022-05-07 12:48:45 +0200

    llamada a la api TTS y generación del archivo mp3 correspondiente

---
+++ lib/api/api_wrapper.dart
@@ -0,0 +1,79 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
+class ApiWrapper {
+  // static ApiWrapper _instance;
+
+  // ApiWrapper._internal() {
+  //   _instance = this;
+  // }
+
+  // factory ApiWrapper() => _instance ?? ApiWrapper._internal();
+
+  final String endpoint = "https://texttospeech.googleapis.com";
+
+  final String route =
+      "v1/text:synthesize?key=AIzaSyAKame4JnmCtac6GazF_aUv5GZpsjCbpPE";
+
+  // http.Response response =
+  //     await http.get(Uri.http(endpoint, api), headers: <String, String>{
+  //   'x-auth-token': authToken,
+  // });
+
+  Future<String> getFrase() async {
+    String datafile = await rootBundle.loadString('frases/frase.txt');
+    return datafile;
+  }
+
+  Future<File> writeMP3(String data) async {
+    final file = await File('audio/decoded.mp3');
+
+    // Write the file
+    return file.writeAsString(data);
+  }
+
+  Future<String> registrarUsuario(String file_path) async {
+    var frase = getFrase();
+
+    http.Response response = await http.post(
+      Uri.http(endpoint, route),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        "input": {"text": frase},
+        "voice": {
+          "languageCode": "es-ES",
+          // "name": "en-GB-Standard-A",
+          "name": "es-ES-Standard-A",
+          "ssmlGender": "FEMALE"
+        },
+        "audioConfig": {"audioEncoding": "MP3"}
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print("usuario registrado");
+      print(response.body.toString());
+      var token = response.body.toString();
+      print('token:$token');
+      Iterable l = json.decode(response.body);
+      var encoded = l.first;
+      // var prods = List<Product>.from(l = Product.fromJson(model)));
+      // String credentials = "username:password";
+      Codec<String, String> stringToBase64 = utf8.fuse(base64);
+      // String encoded = stringToBase64.encode(credentials);      // dXNlcm5hbWU6cGFzc3dvcmQ=
+      String decoded = stringToBase64.decode(encoded);
+      writeMP3(decoded);
+
+      return decoded;
+    } else {
+      print(response.statusCode.toString());
+      print(response.body.toString());
+      return "Error";
+    }
+  }
+}
