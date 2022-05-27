f25ecd2fbe77a023256d4d63ded098873997f202
is-cama <104384688+is-cama@users.noreply.github.com>
2022-05-27 18:54:41 +0200

    Update text2braille.dart
    New comments in text2braille

--- lib/text2braille.dart
+++ lib/text2braille.dart
@@ -11,25 +11,28 @@ import 'package:sm_app_invidente/globals.dart' as globals;
 import 'package:http/http.dart' as http;
 
 class text2brailleState {
+  //Llamada a la API de traductor a braille.
+  //Del json response de la API solo nos interesa el contenido de translated en contents, que correspondería a la traducción en braille.
+  //El contenido de la traducción corresponde a una lista dinámica, como nos interesa tenerlo como string para luego visualizarlo por pantalla todo junto
+  //lo guardamos en una lista string.
+  //Cada posición de la lista string corresponde a una letra/puntuación en braille
+  //La función devuelve una lista string (Future) con el resultado de la traducción
   Future<List<String>> t2bApiCall() async {
-    //Guardamos el texto obtenido
     var frase = globals.resultadoTexto;
 
-    //Nos conectamos a la API de traductor a braille
     final uri = Uri.parse(
         'https://api.funtranslations.com/translate/braille/unicode.json')
         .replace(queryParameters: {
-      'text': frase, //Introducimos como parametro el texto a traducir
+      'text': frase,
     });
-    final response = await http.get(uri); //Esperamos la respuesta
+    final response = await http.get(uri);
 
-    //Creamos lista string vacía para guardar el resultado final de la traducción
     List<String> res = [];
     if (response.statusCode == 200) {
-      print("Response OK"); //Para asegurarnos de que se ha recibido bien la respuesta
-      var data = json.decode(response.body); //Guardamos la información recibida con la respuesta
-      var trans = data["contents"]["translated"]; //Accedemos solo a la traducción a braille
-      res = new List<String>.from(trans); //Pasamos la traducción a braille de lista dinámica a lista string, que es lo que devolverá esta función
+      print("Response OK");
+      var data = json.decode(response.body);
+      var trans = data["contents"]["translated"];
+      res = new List<String>.from(trans);
     } else {
       print(response.statusCode.toString());
       print(response.body.toString());
